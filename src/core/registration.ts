3d768b3f0a489da6c3c9c5c9da4a0694471c8234
Alexander Nazarov <alxkrekeris@gmail.com>
2020-08-23 17:43:29 +0300

    added text to template

--- src/core/registration.ts
+++ src/core/registration.ts
@@ -1,19 +1,23 @@
 export interface IToken {
   name: string;
+  text: string;
   token: string;
 };
 
 export const tokens: IToken[] = [
   {
     name: 'Прізвище',
+    text: 'Будь ласка, введіть своє прізвище (наприклад: Фітільов)',
     token: 'last_name',
   },
   {
     name: 'Ім\'я',
+    text: 'Введіть своє ім\'я (наприклад: Микола) ',
     token: 'first_name',
   },
   {
     name: 'По батькові',
+    text: 'Введіть своє по батькові (наприклад: Григорович)',
     token: 'father_name',
   },
 ];
