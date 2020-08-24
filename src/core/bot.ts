1659c54ece9697c373e34f595b9565e2c0570e28
Alexander Nazarov <alxkrekeris@gmail.com>
2020-08-24 13:57:24 +0300

    sendMessage fix

--- src/core/bot.ts
+++ src/core/bot.ts
@@ -5,7 +5,7 @@ const BOT_API = process.env.BOT_API;
 const API_TOKEN = process.env.BOT_API_TOKEN;
 
 export const sendMessage = (id: string, text: string, parseMode: 'HTML' | 'Markdown' = undefined) => {
-  axios.post(`${BOT_API}/sendMessage`, {
+  axios.post(`${BOT_API}/sendMessage`, {}, {
     params: {
       uid: id,
       text,
