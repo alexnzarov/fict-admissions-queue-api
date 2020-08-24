9f7e53daca763350831ecd9f760bcc9bcb64c226
Alexander Nazarov <alxkrekeris@gmail.com>
2020-08-24 13:40:04 +0300

    added auth to bot request

--- src/core/bot.ts
+++ src/core/bot.ts
@@ -2,6 +2,7 @@ import axios from 'axios';
 import logger from './logger';
 
 const BOT_API = process.env.BOT_API;
+const API_TOKEN = process.env.BOT_API_TOKEN;
 
 export const sendMessage = (id: string, text: string, parseMode: 'HTML' | 'Markdown' = undefined) => {
   axios.get(`${BOT_API}/sendMessage`, {
@@ -9,6 +10,9 @@ export const sendMessage = (id: string, text: string, parseMode: 'HTML' | 'Markd
       id,
       text,
       parse_mode: parseMode,
+    },
+    headers: {
+      Authorization: `Bearer ${API_TOKEN}`,
     },
   }).catch((e) => logger.error('Failed to send Telegram message', { id, text, error: e.toString() }));
 };
