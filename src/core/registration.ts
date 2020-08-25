769912a2c3be9dfced86927815c6232bb83057bd
Alexander Nazarov <alxkrekeris@gmail.com>
2020-08-25 22:57:29 +0300

    added optional params

--- src/core/registration.ts
+++ src/core/registration.ts
@@ -2,6 +2,8 @@ export interface IToken {
   name: string;
   text: string;
   token: string;
+  optional: boolean;
+  values?: string[];
 };
 
 export const tokens: IToken[] = [
@@ -9,15 +11,38 @@ export const tokens: IToken[] = [
     name: 'Прізвище',
     text: 'Будь ласка, введіть своє прізвище (наприклад: Фітільов)',
     token: 'last_name',
+    optional: false,
   },
   {
     name: 'Ім\'я',
     text: 'Введіть своє ім\'я (наприклад: Микола) ',
     token: 'first_name',
+    optional: false,
   },
   {
     name: 'По батькові',
     text: 'Введіть своє по батькові (наприклад: Григорович)',
     token: 'father_name',
+    optional: false,
+  },
+
+  {
+    name: 'Стать',
+    text: 'Будь ласка, вкажіть вашу стать\n\n<i>Ваші особисті дані будуть використані лише для формування заявки на вступ.</i>',
+    token: 'gender',
+    values: ['Чоловіча', 'Жіноча'],
+    optional: true,
+  },
+  {
+    name: 'Пошта',
+    text: 'Вкажіть вашу електронну пошту (наприклад: john@gmail.com)\n\n<i>Ваші особисті дані будуть використані лише для формування заявки на вступ.</i>',
+    token: 'phone_number',
+    optional: true,
+  },
+  {
+    name: 'Телефон',
+    text: 'Вкажіть ваш номер телефону (наприклад: +380961234567)\n\n<i>Ваші особисті дані будуть використані лише для формування заявки на вступ.</i>',
+    token: 'phone_number',
+    optional: true,
   },
 ];
